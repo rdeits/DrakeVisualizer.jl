83a1164a4a42650836a597da400e9d78c9ecc024
Robin Deits <robin.deits@gmail.com>
2016-09-08 14:02:48 -0400

    disable notebook test

--- test/runtests.jl
+++ test/runtests.jl
@@ -77,9 +77,9 @@ let
     Visualizer(cylinder)
 end
 
-let
-    demo_file = "../demo.ipynb"
-    run(`jupyter nbconvert --to notebook --execute $(demo_file) --output $(demo_file)`)
-end
+# let
+#     demo_file = "../demo.ipynb"
+#     run(`jupyter nbconvert --to notebook --execute $(demo_file) --output $(demo_file)`)
+# end
 
 kill(proc)
