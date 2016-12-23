eed27c8b380eb8ca943a9a9d325d61949ad3cb36
Robin Deits <robin.deits@gmail.com>
2016-12-23 03:52:16 -0500

    first working load/draw commands

--- src/json_conversions.jl
+++ src/json_conversions.jl
@@ -1,12 +1,12 @@
 import Base: isempty
 
-
 type GeometryData{GeometryType <: AbstractGeometry, TransformType <: Transformation}
     geometry::GeometryType
     transform::TransformType
     color::RGBA{Float64}
-    name::String
 end
+
+typealias Link Dict{String, GeometryData}
 
 abstract Command
 immutable Delete <: Command
@@ -19,28 +19,55 @@ immutable Draw <: Command
 end
 command_name(::Draw) = "draw"
 
-type PendingCommands
+immutable CommandQueue
     delete::Set{String}
     load::Set{String}
     draw::Set{String}
+
+    CommandQueue() = new(Set{String}(), Set{String}(), Set{String}())
 end
 
 immutable Visualizer
+    lcm::LCM
     links::OrderedDict{String, Link}
     transforms::OrderedDict{String, Transformation}
-    prefix::String
-    queue::PendingCommands
-    lcm::LCM
+    queue::CommandQueue
+
+    function Visualizer(lcm::LCM=LCM())
+        vis = new(lcm,
+                    OrderedDict{String, Link}(),
+                    OrderedDict{String, Transformation}(),
+                    CommandQueue())
+        function handle_msg(channel, msg)
+            onresponse(vis, JSON.parse(msg[:data]))
+        end
+        subscribe(lcm, "DRAKE_VIEWER2_RESPONSE", handle_msg, drakevis[:viewer2_comms_t])
+        vis
+    end
 end
 
-function queue_load!(vis::Visualizer, path::String, link::Link)
+function queue_load!(vis::Visualizer, path::AbstractString)
+    push!(vis.queue.load, path)
+end
+
+function queue_load!(vis::Visualizer, path::AbstractString, link::Link)
     vis.links[path] = link
     push!(vis.queue.load, path)
+end
+
+function load!(vis::Visualizer, path::AbstractString, link::Link)
+    queue_load!(vis, path, link)
+    publish!(vis)
 end
 
 function queue_draw!(vis::Visualizer, path::String, tform)
     vis.transforms[path] = tform
     push!(vis.queue.draw, path)
+end
+
+function draw!(vis::Visualizer, path::String, tform)
+    queue_draw!(vis, path, tform)
+    publish!(vis)
 end
 
 function queue_delete!(vis::Visualizer, path::String)
@@ -49,37 +76,59 @@ function queue_delete!(vis::Visualizer, path::String)
     push!(vis.queue.delete, path)
 end
 
-isempty(queue::PendingCommands) = isempty(queue.delete) && isempty(queue.load) && isempty(queue.draw)
+isempty(queue::CommandQueue) = isempty(queue.delete) && isempty(queue.load) && isempty(queue.draw)
 
 function publish!(vis::Visualizer)
-    publish!(vis, Delete(), vis.queue.delete)
-    publish!(vis, Load(), vis.queue.load)
-    publish!(vis, Draw(), vis.queue.draw)
+    for i in 1:5
+        publish!(vis, Delete(), vis.queue.delete)
+        publish!(vis, Load(), vis.queue.load)
+        publish!(vis, Draw(), vis.queue.draw)
+        PyCall.pycall(vis.lcm.lcm_obj[:handle_timeout], PyCall.PyObject, 100)
+        @show vis.queue
+        if isempty(vis.queue)
+            break
+        end
+    end
 end
 
 function onresponse(vis::Visualizer, msgdata)
+    @show msgdata
+    data = msgdata["data"]
     empty = []
-    for path in get(msgdata, "missing_paths", empty)
-        queue_load!(vis, path)
+    for path in get(data, "missing_paths", empty)
+        queue_load!(vis, join(path, '/'))
     end
-    for path in get(msgdata, "drew_paths", empty)
-        delete!(vis.queue.draw, path)
+    for path in get(data, "drew_paths", empty)
+        delete!(vis.queue.draw, join(path, '/'))
     end
-    for path in get(msgdata, "loaded_paths", empty)
-        delete!(vis.queue.load, path)
+    for path in get(data, "loaded_paths", empty)
+        println("deleting load for path: ", path)
+        delete!(vis.queue.load, join(path, '/'))
     end
-    for path in get(msgdata, "deleted_paths", empty)
-        delete!(vis.queue.delete, path)
-    end
-    if !isempty(vis.queue)
-        publish!(vis)
+    for path in get(data, "deleted_paths", empty)
+        delete!(vis.queue.delete, join(path, '/'))
     end
 end
 
+function to_lcm(data::Associative)
+    msg = drakevis[:viewer2_comms_t]()
+    msg[:timestamp] = data["timestamp"]
+    msg[:format] = "viewer2_json"
+    msg[:format_version_major] = 1
+    msg[:format_version_minor] = 0
+    encoded = JSON.json(data)
+    @show encoded
+    msg[:num_bytes] = length(encoded)
+    msg[:data] = encoded
+    msg
+end
+
 function publish!(vis::Visualizer, cmd::Command, queue)
-    data = serialize(vis, cmd, queue)
-    msg = to_lcm(data)
-    publish(vis.lcm, "DRAKE_VIEWER2_REQUEST", msg)
+    if !isempty(queue)
+        data = serialize(vis, cmd, queue)
+        msg = to_lcm(data)
+        publish(vis.lcm, "DRAKE_VIEWER2_REQUEST", msg)
+    end
 end
 
 function serialize(vis::Visualizer, cmd::Command, queue)
@@ -91,34 +140,38 @@ function serialize(vis::Visualizer, cmd::Command, queue)
     )
 end
 
-fullpath(vis::Visualizer, path::AbstractString) = join((vis.prefix, path), '/')
-
 function command_data(vis::Visualizer, cmd::Delete, queue)
-    Dict("paths" => [fullpath(vis, p) for p in queue])
+    Dict("paths" => collect(queue))
 end
 
 function command_data(vis::Visualizer, cmd::Load, queue)
-    links = [serialize(fullpath(vis, p), vis.links[p]) for p in queue]
+    links = [serialize(p, vis.links[p]) for p in queue]
     Dict("links" => links)
 end
 
+serialize(color::RGBA) = [color.r, color.g, color.b, color.alpha]
+
 function serialize(path::String, link::Link)
-    Dict("path" => path,
-        "geometries" => serialize.(link))
+    geometries = [serialize(name, geom) for (name, geom) in link]
+    Dict("path" => split(path, '/'),
+        "geometries" => geometries)
 end
 
-function serialize(geomdata::GeometryData)
+function serialize(name::AbstractString, geomdata::GeometryData)
     params = serialize(geomdata.geometry)
     intrinsic_tform = intrinsic_transform(geomdata.geometry)
-    Dict("name" => geomdata.name,
+    Dict("name" => name,
          "color" => serialize(geomdata.color),
          "pose" => serialize(compose(geomdata.transform, intrinsic_tform)),
          "parameters" => params)
 end
 
-intrinsic_transform(geom::AbstractGeometry) = Translation(center(geometry)...)
+intrinsic_transform(geom::AbstractGeometry) = Translation(center(geom)...)
 
-serialize(g::HyperRectangle) = Dict("type" => "box", "lengths" => widths(g))
+serialize(v::Vec) = convert(Vector, v)
+serialize(v::Point) = convert(Vector, v)
+
+serialize(g::HyperRectangle) = Dict("type" => "box", "lengths" => serialize(widths(g)))
 serialize(g::HyperSphere) = Dict("type" => "sphere", "radius" => radius(g))
 serialize(g::HyperEllipsoid) = Dict("type" => "ellipsoid", "radii" => radii(g))
 serialize(g::HyperCylinder{3}) = Dict("type" => "cylinder",
@@ -128,12 +181,12 @@ serialize(g::HyperCube) = Dict("type" => "box", "lengths" => widths(g))
 
 
 function command_data(vis::Visualizer, cmd::Draw, queue)
-    commands = [serialize(fullpath(vis, p), vis.transforms[p]) for p in queue]
+    commands = [serialize(p, vis.transforms[p]) for p in queue]
     Dict("commands" => commands)
 end
 
 function serialize(path::String, tform::Transformation)
-    Dict("path" => path,
+    Dict("path" => split(path, '/'),
         "pose" => serialize(tform))
 end
 
