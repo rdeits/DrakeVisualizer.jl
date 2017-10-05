bb1806ef9d2084924f1f732bce92368d7a72921f
Twan Koolen <koolen.twan@gmail.com>
2017-10-04 21:44:31 -0400

    Only run script test when DISPLAY is set.

--- test/testscript.py
+++ test/testscript.py
@@ -1,13 +1,5 @@
-from director import lcmUtils
-from director.utime import getUtime
-import robotlocomotion as lcmrl
+import os
 
-channel = 'DRAKE_VISUALIZER_JL_TEST'
-msg = lcmrl.viewer2_comms_t()
-msg.utime = getUtime()
-msg.format = 'drake_visualizer_jl_test'
-msg.format_version_major = 0
-msg.format_version_minor = 1
-msg.data = bytearray()
-msg.num_bytes = len(msg.data)
-lcmUtils.publish(channel, msg)
+fname = "test_script_success"
+with open(fname, 'a'):
+    os.utime(fname, None)
