8370e5e85fb06ff05d8483ba0dd0dd5644d986b6
Twan Koolen <koolen.twan@gmail.com>
2018-01-18 14:47:01 -0500

    Use LCMCore.LCMType for CommsT.

--- src/lcmtypes/comms_t.jl
+++ src/lcmtypes/comms_t.jl
@@ -1,50 +1,20 @@
 module Comms
 
-import LCMCore: encode, decode
+using LCMCore
+using StaticArrays
 
-struct CommsT
+mutable struct CommsT <: LCMType
     utime::Int64
     format::String
     format_version_major::Int32
     format_version_minor::Int32
+    num_bytes::Int32
     data::Vector{UInt8}
 end
 
-fingerprint(::Type{CommsT}) = UInt8[211, 104, 224, 63, 51, 197, 104, 190]
-
-function encode(comms::CommsT)
-    io = IOBuffer()
-    write(io, fingerprint(CommsT))
-    write(io, hton(comms.utime))
-    write(io, hton(UInt32(length(comms.format) + 1)))
-    write(io, comms.format)
-    write(io, UInt8(0))
-    write(io, hton(comms.format_version_major))
-    write(io, hton(comms.format_version_minor))
-    write(io, hton(Int32(length(comms.data))))
-    write(io, hton.(comms.data))
-    io.data
-end
-
-struct FingerprintException <: Exception
-    message::String
-end
-
-function decode(data::Vector{UInt8}, ::Type{CommsT})
-    io = IOBuffer(data)
-    msg_fingerprint = read(io, 8)
-    if msg_fingerprint != fingerprint(CommsT)
-        throw(FingerprintException("LCM message fingerprint did not match. This means that you are trying to decode the wrong message type, or a different version of the message type."))
-    end
-    utime = Int64(ntoh(read(io, Int64)))
-    formatlen = UInt32(ntoh(read(io, UInt32)))
-    format = String(read(io, formatlen - 1))
-    read(io, 1) # strip off null
-    ver_maj = Int32(ntoh(read(io, Int32)))
-    ver_min = Int32(ntoh(read(io, Int32)))
-    num_bytes = Int32(ntoh(read(io, Int32)))
-    data = String(read(io, UInt8, num_bytes))
-    CommsT(utime, format, ver_maj, ver_min, data)
-end
+LCMCore.fingerprint(::Type{CommsT}) = SVector(0xd3, 0x68, 0xe0, 0x3f, 0x33, 0xc5, 0x68, 0xbe)
+LCMCore.size_fields(::Type{CommsT}) = (:num_bytes,)
+LCMCore.check_valid(x::CommsT) = @assert length(x.data) == x.num_bytes
+Base.resize!(x::CommsT) = resize!(x.data, x.num_bytes)
 
 end
